d3628c0c2b72a97bb3a6f0d3a6a37314e8801c09
Neil Harries <neilh789@protonmail.com>
2023-02-10 13:26:28 +0100

    Update app.py
    Removed templating

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
 import requests
-#import json
-#from flask import render_template
 
 # Create Application
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,10 +16,10 @@ def hello_world():
 @app.route("/country")
 def get_country():
     ip = requests.get('https://api.ipify.org').text
+    print(ip)
     response = requests.get(f'https://ipapi.co/{ip}/json/').json()
     country = response['country_name']
     return f"Hello from {country}"
-    #return render_template('IPCountry.html', ip=ip,country=country)
 
 if __name__ == "__main__":
     app.debug = True
