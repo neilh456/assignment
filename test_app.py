927e0ec39b9ba6b35ccba6e0fdbe91eb3e95e045
Neil Harries <neilh789@protonmail.com>
2023-02-10 12:18:12 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,6 +2,6 @@ import app
 
 def test_flask_app():
     with app.app.test_client() as client:
-        response = client.get("/")
+        response = client.get("/hello")
         assert response.status_code == 200
         assert response.data == b"Hello World"
