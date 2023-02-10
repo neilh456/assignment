0633505195e99f9635c0a6ca604f81bc19d8f829
Neil Harries <neilh789@protonmail.com>
2023-02-10 12:15:04 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,12 +1,7 @@
-import pytest
-import requests
+import app
 
 def test_flask_app():
-    # Make a GET request to the Flask app endpoint
-    response = requests.get('http://localhost:5000/')
-
-    # Check that the response status code is 200 (success)
-    assert response.status_code == 200
-
-    # Check that the response contains the expected message
-    assert b'Hello World' in response.content
+    with app.app.test_client() as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.data == b"Hello World"
